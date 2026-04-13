a043afd2fbb56bd0cf7f001af8993c93a647bcf3
David Bankson <62538124+QuothTheRaven42@users.noreply.github.com>
2026-04-13 00:07:05 -0700

    Update magic_the_gathering_api_lookup.py

--- magic_the_gathering_api_lookup.py
+++ magic_the_gathering_api_lookup.py
@@ -1,11 +1,13 @@
 import requests
 
-def card_lookup(card_name):
+
+def card_lookup(card_name: str) -> tuple:
     url = "https://api.scryfall.com/cards/named?fuzzy="
     url += card_name
     response = requests.get(url)
     card = response.json()
     return response, card
+
 
 def main():
     while True:
@@ -20,16 +22,15 @@ def main():
             print(f"\n{card['name']}\nMana cost: {card['mana_cost']}")
 
             # check for multi-faced cards like Gleemax
-            if 'oracle_text' in card:
+            if "oracle_text" in card:
                 print(f"{card['oracle_text']}")
             else:
-                for index in range(len(card['card_faces'])):
-                  print(f"Side {index+1}: {card['card_faces'][index]['oracle_text']}")
-            print(f"{card['type_line']}"
-                  f"\n{card['set_name']}")
+                for index in range(len(card["card_faces"])):
+                    print(f"Side {index+1}: {card['card_faces'][index]['oracle_text']}")
+            print(f"{card['type_line']}" f"\n{card['set_name']}")
 
             # checks if creature
-            if 'power' in card and 'toughness' in card:
+            if "power" in card and "toughness" in card:
                 print(f"Power/Toughness: {card['power']}/{card['toughness']}")
 
         elif response.status_code == 404:
@@ -38,5 +39,6 @@ def main():
         else:
             print(f"Failed with status code: {response.status_code}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
